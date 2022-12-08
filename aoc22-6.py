51971eb4061f4133479da5033d394a18bc7ac451
KoenKahlman <k.kahlman@student.vu.nl>
2022-12-08 11:48:09 +0100

    bug

--- aoc22-6.py
+++ aoc22-6.py
@@ -1,7 +1,7 @@
 with open("input6.txt") as input:
   for line in input.readlines():
     windowsize = 4
-    for i in range(3,len(line)):
+    for i in range(windowsize-1,len(line)):
       if len(set(line[i-windowsize:i])) == windowsize:
         print("Part 1: "+str(i))
         break
@@ -9,7 +9,7 @@ with open("input6.txt") as input:
 with open("input6.txt") as input:
   for line in input.readlines():
     windowsize = 14
-    for i in range(3,len(line)):
+    for i in range(windowsize-1,len(line)):
       if len(set(line[i-windowsize:i])) == windowsize:
         print("Part 2: "+str(i))
         break
